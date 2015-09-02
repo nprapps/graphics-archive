c21698b1d50d181da204f962f2306d8cc74d76a7
Alyson Hurt <ahurt@npr.org>
2015-09-02 17:30:24 -0400

    archiving

---
+++ 2015/05/abortion-party-20150529/graphic_config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+COPY_GOOGLE_DOC_KEY = '1P_eY0qPA7Rk2nBFC9-1cX2mrJfiMjtaMUAIwFWtUde0'
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
---
+++ 2015/05/abortion-support-20150529/graphic_config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+COPY_GOOGLE_DOC_KEY = '1nTN8sIUGtj8c6d64dgGR8dBdGKExTMcH_53yFxmONlo'
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
