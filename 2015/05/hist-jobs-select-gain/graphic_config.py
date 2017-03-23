9d797b560ec316a4b0a67897b5de77276ae6efd9
jjelosua <jelosua@npr.org>
2017-03-23 11:38:47 -0400

    change to accesible COPY

--- 2015/05/hist-jobs-select-gain/graphic_config.py
+++ 2015/05/hist-jobs-select-gain/graphic_config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-COPY_GOOGLE_DOC_KEY = '1GtziTUWxm18K7go_5IB60sToZHB1saCKBMphmh3eZ2A'
-
+# COPY_GOOGLE_DOC_KEY = '1GtziTUWxm18K7go_5IB60sToZHB1saCKBMphmh3eZ2A'
+COPY_GOOGLE_DOC_KEY = '1eUNUWh7rHHtggbpXHrXY39x8kzVgIJ-dx4DaKx1ooTU'
 # Use these variables to override the default cache timeouts for this graphic
 # DEFAULT_MAX_AGE = 20
 # ASSETS_MAX_AGE = 300
