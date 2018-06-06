51e73cec3cd755570f7074293de2b7795b9a839b
Alyson Hurt <ahurt@npr.org>
2018-06-05 22:31:50 -0400

    archive remaining march 2018 graphics

---
+++ 2018/03/tech-stocks-20180329/graphic_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import base_filters
+
+COPY_GOOGLE_DOC_KEY = '1pR5y5iWjbW1AbhySd5THVJfv4lLBmEOAZrENehrZ550'
+
+USE_ASSETS = False
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
+
+JINJA_FILTER_FUNCTIONS = base_filters.FILTERS
---
+++ 2018/03/trade-balance-20180328/js/graphic.js
@@ -0,0 +1,329 @@
+// Global vars
+var pymChild = null;
+var isMobile = false;
+var dataSeries = [];
+
+var recession_dates = [
+    { 'begin': '2007-12-01', 'end': '2009-06-01' }
+];
+
+/*
+ * Initialize graphic
+ */
+var onWindowLoaded = function() {
+    if (Modernizr.svg) {
+        formatData();
+
+        pymChild = new pym.Child({
+            renderCallback: render
+        });
+    } else {
+        pymChild = new pym.Child({});
+    }
+
+    pymChild.onMessage('on-screen', function(bucket) {
+        ANALYTICS.trackEvent('on-screen', bucket);
+    });
+    pymChild.onMessage('scroll-depth', function(data) {
+        ANALYTICS.trackEvent('scroll-depth', data.percent, data.seconds);
+    });
+}
+
+/*
+ * Format graphic data for processing by D3.
+ */
+var formatData = function() {
+    DATA.forEach(function(d) {
+        d['date'] = d3.time.format('%Y').parse(d['date']);
+        d['amt'] = +d['amt'];
+    });
+
+    recession_dates.forEach(function(d) {
+        d['begin'] = d3.time.format('%Y-%m-%d').parse(d['begin']);
+        d['end'] = d3.time.format('%Y-%m-%d').parse(d['end']);
+    });
+
+    /*
+     * Restructure tabular data for easier charting.
+     */
+    dataSeries.push({
+        'name': 'deficit',
+        'values': DATA.map(function(d) {
+            return {
+                'date': d['date'],
+                'amt': d['amt']
+            };
+        })
+    });
+    
+}
+
+/*
+ * Render the graphic(s). Called by pym with the container width.
+ */
+var render = function(containerWidth) {
+    if (!containerWidth) {
+        containerWidth = DEFAULT_WIDTH;
+    }
+
+    if (containerWidth <= MOBILE_THRESHOLD) {
+        isMobile = true;
+    } else {
+        isMobile = false;
+    }
+
+    // Render the chart!
+    renderLineChart({
+        container: '#line-chart',
+        width: containerWidth,
+        data: dataSeries
+    });
+
+    // Update iframe
+    if (pymChild) {
+        pymChild.sendHeight();
+    }
+}
+
+/*
+ * Render a line chart.
+ */
+var renderLineChart = function(config) {
+    /*
+     * Setup
+     */
+    var dateColumn = 'date';
+    var valueColumn = 'amt';
+
+    var aspectWidth = isMobile ? 4 : 16;
+    var aspectHeight = isMobile ? 3 : 9;
+
+    var margins = {
+        top: 5,
+        right: 82,
+        bottom: 20,
+        left: 67
+    };
+
+    var ticksX = 10;
+    var ticksY = 10;
+    var roundTicksFactor = 100000000000;
+
+    // Mobile
+    if (isMobile) {
+        ticksX = 5;
+        ticksY = 5;
+        margins['left'] = 45;
+        margins['right'] = 45;
+    }
+
+    // Calculate actual chart dimensions
+    var chartWidth = config['width'] - margins['left'] - margins['right'];
+    var chartHeight = Math.ceil((config['width'] * aspectHeight) / aspectWidth) - margins['top'] - margins['bottom'];
+
+    // Clear existing graphic (for redraw)
+    var containerElement = d3.select(config['container']);
+    containerElement.html('');
+
+    /*
+     * Create D3 scale objects.
+     */
+    var xScale = d3.time.scale()
+        .domain(d3.extent(config['data'][0]['values'], function(d) {
+            return d['date'];
+        }))
+        .range([ 0, chartWidth ])
+
+    var min = d3.min(config['data'], function(d) {
+        return d3.min(d['values'], function(v) {
+            return Math.floor(v[valueColumn] / roundTicksFactor) * roundTicksFactor;
+        })
+    });
+
+    if (min > 0) {
+        min = 0;
+    }
+
+    var max = d3.max(config['data'], function(d) {
+        return d3.max(d['values'], function(v) {
+            return Math.ceil(v[valueColumn] / roundTicksFactor) * roundTicksFactor;
+        })
+    });
+
+    var yScale = d3.scale.linear()
+        .domain([min, max])
+        .range([chartHeight, 0]);
+
+    var colorScale = d3.scale.ordinal()
+        .domain(_.pluck(config['data'], 'name'))
+        .range([COLORS['red3'], COLORS['yellow3'], COLORS['blue3'], COLORS['orange3'], COLORS['teal3']]);
+
+    /*
+     * Create the root SVG element.
+     */
+    var chartWrapper = containerElement.append('div')
+        .attr('class', 'graphic-wrapper');
+
+    var chartElement = chartWrapper.append('svg')
+        .attr('width', chartWidth + margins['left'] + margins['right'])
+        .attr('height', chartHeight + margins['top'] + margins['bottom'])
+        .append('g')
+        .attr('transform', 'translate(' + margins['left'] + ',' + margins['top'] + ')');
+
+    /*
+     * Create D3 axes.
+     */
+    var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient('bottom')
+        .ticks(ticksX)
+        .tickFormat(function(d, i) {
+            if (isMobile) {
+                return '\u2019' + fmtYearAbbrev(d);
+            } else {
+                return fmtYearFull(d);
+            }
+        });
+
+    var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient('left')
+        .ticks(ticksY)
+        .tickFormat(function(d) {
+            if (d === 0) {
+                return '$' + d;
+            } else {
+                if (isMobile) {
+                    return '$' + d / 1000000000 + 'B';
+                } else {
+                    return '$' + d / 1000000000 + ' billion';
+                }
+            }
+        });
+
+    var recession = chartElement.append('g')
+        .attr('class', 'recession')
+        .selectAll('rect')
+        .data(recession_dates)
+        .enter()
+            .append('rect')
+                .attr('x', function(d) {
+                    return xScale(d['begin']);
+                })
+                .attr('width', function(d) {
+                    return xScale(d['end']) - xScale(d['begin']);
+                })
+                .attr('y', 0)
+                .attr('height', chartHeight)
+                .attr('fill', '#ebebeb');
+
+    /*
+     * Render axes to chart.
+     */
+    chartElement.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', makeTranslate(0, chartHeight))
+        .call(xAxis);
+
+    chartElement.append('g')
+        .attr('class', 'y axis')
+        .call(yAxis);
+
+    /*
+     * Render grid to chart.
+     */
+    var xAxisGrid = function() {
+        return xAxis;
+    }
+
+    var yAxisGrid = function() {
+        return yAxis;
+    }
+
+    chartElement.append('g')
+        .attr('class', 'x grid')
+        .attr('transform', makeTranslate(0, chartHeight))
+        .call(xAxisGrid()
+            .tickSize(-chartHeight, 0, 0)
+            .tickFormat('')
+        );
+
+    chartElement.append('g')
+        .attr('class', 'y grid')
+        .call(yAxisGrid()
+            .tickSize(-chartWidth, 0, 0)
+            .tickFormat('')
+        );
+
+    /*
+     * Render lines to chart.
+     */
+    var line = d3.svg.line()
+        .interpolate('monotone')
+        .x(function(d) {
+            return xScale(d[dateColumn]);
+        })
+        .y(function(d) {
+            return yScale(d[valueColumn]);
+        });
+
+    chartElement.append('g')
+        .attr('class', 'lines')
+        .selectAll('path')
+        .data(config['data'])
+        .enter()
+        .append('path')
+            .attr('class', function(d, i) {
+                return 'line ' + classify(d['name']);
+            })
+            .attr('stroke', function(d) {
+                return colorScale(d['name']);
+            })
+            .attr('d', function(d) {
+                return line(d['values']);
+            });
+
+    // draw recession label
+    chartElement.append('text')
+        .classed('chart-label', true)
+        .attr('x', function(){
+            var dates = recession_dates[0];
+            return xScale(dates['begin']) + ((xScale(dates['end']) - xScale(dates['begin'])) / 2);
+        })
+        .attr('y', isMobile ? 5 : 15)
+        .text('Recession');
+
+    // label the last value in the series
+    chartElement.append('g')
+        .attr('class', 'value')
+        .selectAll('text')
+        .data(config['data'])
+        .enter().append('text')
+            .attr('x', function(d, i) {
+                var last = d['values'][d['values'].length - 1];
+
+                return xScale(last[dateColumn]) + 5;
+            })
+            .attr('y', function(d) {
+                var last = d['values'][d['values'].length - 1];
+
+                return yScale(last[valueColumn]) + 3;
+            })
+            .text(function(d) {
+                var last = d['values'][d['values'].length - 1];
+                var value = last[valueColumn];
+
+                if (isMobile) {
+                    var label = '$' + (last[valueColumn] / 1000000000).toFixed(1) + 'B';
+                } else {
+                    var label = '$' + (last[valueColumn] / 1000000000).toFixed(1) + ' billion';
+                }
+                return label;
+            });
+}
+
+/*
+ * Initially load the graphic
+ * (NB: Use window.load to ensure all images have loaded)
+ */
+window.onload = onWindowLoaded;
