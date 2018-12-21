fa453b41cf44a5abaec9a7ec1bc74c4cb0864845
alykat <ahurt@npr.org>
2018-12-21 12:29:32 -0500

    move 3 gfx to archive

---
+++ 2018/11/marist-packages-20181031/graphic_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import base_filters
+
+COPY_GOOGLE_DOC_KEY = '14ROZ7AJZdMR5DRCRfe2Ynq0uYv37wz8Fw2-yeQhZ8kg'
+
+USE_ASSETS = False
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
+
+JINJA_FILTER_FUNCTIONS = base_filters.FILTERS
