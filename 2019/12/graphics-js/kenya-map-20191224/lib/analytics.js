9d8b544a7de27746827a5ccbc20664a5f0c707a4
Alyson Hurt <alykat@gmail.com>
2025-06-02 10:56:59 -0400

    kenya-map-20191224: update analytics + data consent

--- 2019/12/graphics-js/kenya-map-20191224/lib/analytics.js
+++ 2019/12/graphics-js/kenya-map-20191224/lib/analytics.js
@@ -3,86 +3,123 @@
  */
 
 var { getParameterByName, urlToLocation } = require("./helpers");
+var DataConsent = require("./dataConsent");
 
 var ANALYTICS = (function () {
-    /*
-     * Google Analytics
-     */
-    var DIMENSION_PARENT_URL = 'dimension1';
-    var DIMENSION_PARENT_HOSTNAME = 'dimension2';
-    var DIMENSION_PARENT_INITIAL_WIDTH = 'dimension3';
+  var googleAnalyticsAlreadyInitialized = false;
 
-    var setupGoogle = function() {
-        (function(i,s,o,g,r,a,m) {
-            i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  /*
+   * Google Analytics
+   */
+  var DIMENSION_PARENT_URL = "dimension1";
+  var DIMENSION_PARENT_HOSTNAME = "dimension2";
+  var DIMENSION_PARENT_INITIAL_WIDTH = "dimension3";
 
-        ga('create', window.GOOGLE_ANALYTICS_ID, 'auto');
+  var setupGoogle = function () {
+    var gtagID = window.GOOGLE_ANALYTICS_ID;
 
-        // By default Google tracks the query string, but we want to ignore it.
-        var location = window.location.protocol +
-            '//' + window.location.hostname +
-            window.location.pathname;
+    // Bail early if opted out of Performance and Analytics consent groups
+    if (!DataConsent.hasConsentedTo(DataConsent.PERFORMANCE_AND_ANALYTICS))
+      return;
 
-        ga('set', 'location', location);
-        ga('set', 'page', window.location.pathname);
+    var script = document.createElement("script");
 
-        // Custom dimensions & metrics
-        var parentUrl = getParameterByName('parentUrl') || '';
-        var parentHostname = '';
+    script.src = "https://www.googletagmanager.com/gtag/js?id=" + gtagID;
 
-        if (parentUrl) {
-            parentHostname = urlToLocation(parentUrl).hostname;
-        }
+    script.async = true;
 
-        var initialWidth = getParameterByName('initialWidth') || '';
+    var script_embed = document.createElement("script");
 
-        var customData = {};
-        customData[DIMENSION_PARENT_URL] = parentUrl;
-        customData[DIMENSION_PARENT_HOSTNAME] = parentHostname;
-        customData[DIMENSION_PARENT_INITIAL_WIDTH] = initialWidth;
+    script_embed.innerHTML =
+      "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '" +
+      gtagID +
+      "', { 'send_page_view': false });";
+    document.head.append(script, script_embed);
 
-        // Track pageview
-        ga('send', 'pageview', customData);
-     }
+    // By default Google tracks the query string, but we want to ignore it.
+    var here = new URL(window.location);
 
-    /*
-     * Event tracking.
-     */
-    var trackEvent = function(eventName, label, value) {
-        var eventData = {
-            'hitType': 'event',
-            'eventCategory': document.title,
-            'eventAction': eventName
-        }
+    // Custom dimensions & metrics
+    var parentUrl = here.searchParams.has("parentUrl")
+      ? new URL(here.searchParams.get("parentUrl"))
+      : "";
+    var parentHostname = "";
 
-        if (label) {
-            eventData['eventLabel'] = label;
-        }
-
-        if (value) {
-            eventData['eventValue'] = value
-        }
-
-        // Track details about the parent with each event
-        var parentUrl = getParameterByName('parentUrl') || '';
-        var parentHostname = '';
-        if (parentUrl) {
-            parentHostname = urlToLocation(parentUrl).hostname;
-        }
-        eventData[DIMENSION_PARENT_URL] = parentUrl;
-        eventData[DIMENSION_PARENT_HOSTNAME] = parentHostname;
-
-        ga('send', eventData);
+    if (parentUrl) {
+      parentHostname = parentUrl.hostname;
     }
 
-    setupGoogle();
+    var initialWidth = here.searchParams.get("initialWidth") || "";
 
-    return {
-        'trackEvent': trackEvent
+    var customData = {};
+    customData["dimension1"] = parentUrl;
+    customData["dimension2"] = parentHostname;
+    customData["dimension3"] = initialWidth;
+
+    // Track pageview
+    gtag("event", "page_view", customData);
+    googleAnalyticsAlreadyInitialized = true;
+  };
+
+  /*
+   * Event tracking.
+   */
+  var trackEvent = function (eventName, label, value) {
+    // Bail early if opted out of Performance and Analytics consent groups
+    if (!DataConsent.hasConsentedTo(DataConsent.PERFORMANCE_AND_ANALYTICS))
+      return;
+
+    var eventData = {
+      eventCategory: document.title,
+      eventAction: eventName,
     };
-}());
+
+    if (label) {
+      eventData["eventLabel"] = label;
+    }
+
+    if (value) {
+      eventData["eventValue"] = value;
+    }
+
+    // Track details about the parent with each event
+    var parentUrl = getParameterByName("parentUrl") || "";
+    var parentHostname = "";
+    if (parentUrl) {
+      parentHostname = urlToLocation(parentUrl).hostname;
+    }
+    eventData[DIMENSION_PARENT_URL] = parentUrl;
+    eventData[DIMENSION_PARENT_HOSTNAME] = parentHostname;
+
+    gtag("event", "dailygraphics", eventData);
+  };
+
+  setupGoogle();
+
+  // Listen for DataConsentChanged event
+  document.addEventListener("npr:DataConsentChanged", () => {
+    // Bail early if GA's already been set up
+    if (googleAnalyticsAlreadyInitialized) return;
+
+    // When a user opts into performance and analytics cookies, initialize GA
+    if (DataConsent.hasConsentedTo(DataConsent.PERFORMANCE_AND_ANALYTICS)) {
+      setupGoogle();
+    }
+  });
+
+  // listen for Data Consent overlay being closed on NPR.org
+  window.addEventListener("message", event => {
+    console.log(event);
+    const origin = /.*npr\.org.*/g;
+    if (event.data == "Data consent updated" && origin.test(event.origin)) {
+      OneTrust.Close();
+      setupGoogle();
+    }
+  });
+
+  return {
+    trackEvent: trackEvent,
+  };
+})();
 
 module.exports = ANALYTICS;
